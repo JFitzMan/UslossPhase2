70777840938c1e212d74e63c526ef1cc1465eecc
JordanFitzpatrick <fitzpatrickj1@email.arizona.edu>
2015-09-30 16:59:27 -0700

    added constant EMPTY = -1 to message.h

--- message.h
+++ message.h
@@ -9,6 +9,7 @@ typedef struct mboxProc *mboxProcPtr;
 struct mailbox {
     int          mboxID;
     int          numSlots;
+    int          slotsInUse;
     int          slotSize;
     slotPtr      firstSlot;
 };
@@ -22,6 +23,7 @@ struct mailSlot {
 
 struct mboxProc{
     int       pid;
+    int       status;
     //pointer to next proc
     //place to store message directly by a mailbox
     //probably a few more, but less than phase 1
@@ -39,3 +41,7 @@ union psrValues {
     struct psrBits bits;
     unsigned int integerPart;
 };
+
+#define EMPTY -1
+
+//probably some blocked on send/recieve ready, various other constants for proc table
